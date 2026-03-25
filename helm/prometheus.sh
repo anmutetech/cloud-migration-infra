c525795fee15d9a509c14054c720fef8a8c09007
fda2026 <jamil.ceo.abiodun@gmail.com>
2026-03-25 14:07:54 +0100

    Forget to remove the file from .sh

--- helm/prometheus.sh
+++ helm/prometheus.sh
@@ -8,7 +8,6 @@ helm repo update
 helm uninstall monitoring -n monitoring
 
 helm install monitoring prometheus-community/kube-prometheus-stack \
--f monitoring/prometheus-values.yaml \
 -f monitoring/values.yaml \
 -n monitoring \
 --create-namespace
